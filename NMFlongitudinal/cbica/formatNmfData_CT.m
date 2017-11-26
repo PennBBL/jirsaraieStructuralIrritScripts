635dbbc30fd5621ad36c3ce83466a96930310046
robertjirsaraie21@gmail.com <rjirsaraie@chead.uphs.upenn.edu>
2017-11-26 02:45:08 -0500

    Creates a spreadsheet of NMF Values

---
+++ NMFlongitudinal/cbica/formatNmfData_CT.m
@@ -0,0 +1,28 @@
+clear all
+
+numBases = 18;
+
+%% read in NMF decomposition
+cd(sprintf('/cbica/projects/pncNmf/grmpyNMF/n229_longitudinal/results/NumBases%d/OPNMF',numBases))
+
+
+load('ResultsExtractBases.mat')
+
+% transpose so format is Nsub X NumComponents
+C_t = C';
+
+% assigning variable names
+pre = sprintf('Ct_Nmf%dC',numBases);
+
+varNames = {};
+for k = 1:numBases
+    varNames = [varNames; strcat([pre,num2str(k,'%d')])];
+end
+
+% convert to table so we can assign variable names
+T = array2table(C_t,'VariableNames',varNames)
+
+% writing table to .csv file 
+writetable(T,sprintf('/cbica/projects/pncNmf/grmpyNMF/n229_longitudinal/results/data/NmfResults%dBases_CT.csv',numBases),'Delimiter',',')
+
+
