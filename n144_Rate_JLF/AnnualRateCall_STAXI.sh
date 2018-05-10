7c1524f241e1a26822a543c36049a530016d6bc5
robertjirsaraie21@gmail.com <rjirsaraie@chead.uphs.upenn.edu>
2018-05-09 20:58:18 -0400

    STATI Analysis Call: Alternative to ARI

---
+++ n144_Rate_JLF/AnnualRateCall_STAXI.sh
@@ -0,0 +1,76 @@
+###################################################################################################
+##########################           GRMPY - Run ROI Wrapper             ##########################
+##########################               Robert Jirsaraie                ##########################
+##########################        rjirsara@pennmedicine.upenn.edu        ##########################
+##########################                 11/18/2017                    ##########################
+###################################################################################################
+#####  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  #####
+###################################################################################################
+<<USE
+
+This version of the ROI Wrapper call used to specifically run repeated measures of longitudinal data 
+(GRMPY & PNC) on JLF regional data.
+
+USE
+###################################################################################################
+#####  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  #####
+###################################################################################################
+
+###############################################
+##### Define the RDS File and Output Path #####
+###############################################
+
+subjDataName="/data/jux/BBL/projects/jirsaraieStructuralIrrit/data/n144_Rate_JLF/n141_Demo+STAXI+QA_20180322.rds"
+
+OutDirRoot="/data/jux/BBL/projects/jirsaraieStructuralIrrit/output/n144_Rate_JLF"  
+
+###################################################################################################
+##### Define the name of the Variable that Specific which Subjects to Exclude in the RDS File #####
+#####         Subjects To Include are Coded as "1" and the Ones to Exclude are "0"            #####
+###################################################################################################
+
+subjID="bblid,scanid"
+
+inclusionName="DeltaEXCLUDE"
+
+#######################################################################################
+##### Define the Formula to Run, including the amount of knots and the Predictors #####
+#####   Do Not Include Method as it will be Defined in gamROI.R (method="REML")   #####    
+#######################################################################################
+
+covsFormula="~TP1ageAtScan1+traitanger_log+DeltaManualRating+sex"
+
+pAdjustMethod="fdr"
+
+ncores=5
+
+residualMap=FALSE
+
+######################################################################
+##### If doing Repeated Measures, Include the Following Argument #####
+######################################################################
+
+#randomFormula="~(1|bblid)"
+
+# Put in Call If using command: -e $randomFormula
+########################################################################
+##### Define Paths of the Neuroimaging CSV's that will be Analyzed #####
+#####  If a varaible is missing more than half the data, then you  #####
+#####   will encounter errors, use reduceGMD.R to remove "NA's"    #####
+########################################################################
+
+input="/data/jux/BBL/projects/jirsaraieStructuralIrrit/data/n144_Rate_JLF/n141_jlfAntsCTIntersectionCTannualRate_20180404.csv"
+
+########################################################################
+##### Calls the " gamROI.R" Rscript that will Perform the Analysis #####
+########################################################################
+
+for i in $input; do 
+
+Rscript /data/jux/BBL/projects/jirsaraieStructuralIrrit/scripts/jirsaraieStructuralIrritScripts/n144_Rate_JLF/gamROI.R  -c $subjDataName -o $OutDirRoot -p ${i} -i $inclusionName -u $subjID -f $covsFormula -a $pAdjustMethod -r $residualMap -n 5 
+
+done
+
+###################################################################################################
+#####  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  #####
+###################################################################################################
